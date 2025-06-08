258dcf0cf57a0811525a055048147c0f9c57bc1d
Shark <cuong28022003@gmail.com>
2025-06-08 14:24:36 +0700

    fix db connection

--- src/main/java/mobile/MobileApplication.java
+++ src/main/java/mobile/MobileApplication.java
@@ -3,6 +3,7 @@ package mobile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -12,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableMongoRepositories
 @EnableMongoAuditing
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude={MongoAutoConfiguration.class})
 public class MobileApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
